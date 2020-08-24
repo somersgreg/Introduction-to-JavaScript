7c62de5cb17d7c76b4099c32016da2084d6d9790
Greg Somers <somersgreg@yahoo.com>
2020-08-23 21:25:30 -0400

    > Task 6 nearly done

--- JS_Tasks/Task_6.js
+++ JS_Tasks/Task_6.js
@@ -4,3 +4,10 @@
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
 
 
+function annoyingSong (num){
+    console.log(num++)
+    for (i=num; i <=0; i--)
+    console.log (`${num}, bottles of soda on the wall, ${num} bottles of soda, take one down pass it around", ${num-1} bottles of soda on the wall`)
+    return num;
+}
+annoyingSong(5)
